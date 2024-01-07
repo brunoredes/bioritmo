e6147d522ca45634dba4a4dd0b6d28b53b89301c
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-01-07 11:37:48 -0300

    feat: remove nx default component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Route } from '@angular/router';
+import { HOME_ROUTES } from './domains/home/home.routes';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', loadChildren: () => HOME_ROUTES },
+];
